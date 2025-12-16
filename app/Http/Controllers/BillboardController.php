d63a92e880718a63c533aab597c25a1e94617c3e
charithawickramasinghe <80460181+charithawickramasinghe@users.noreply.github.com>
2025-12-16 14:15:36 +0100

    fix: Filter the sldeshow images by using users

--- app/Http/Controllers/BillboardController.php
+++ app/Http/Controllers/BillboardController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
 
 class BillboardController extends Controller
 {
     public function show($screen_id)
     {
-        $images = Image::where('screen_id', $screen_id)->get();
+        $userId = Auth::id();
+        $images = Image::where('screen_id', $screen_id)
+                        ->where('user_id', $userId)
+                        ->get();
 
         return view('billboard', compact('images'));
     }
